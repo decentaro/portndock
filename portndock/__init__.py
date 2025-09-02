37c5b00f4d1a817073e65be36c18ddca8ea0b81f
decentaro <dymarc92@gmail.com>
2025-09-02 08:53:31 -0700

    Bump to v1.0.2 - macOS compatibility fix
    - Same code as TestPyPI v1.0.1 with shutil import fix
    - Resolves PyPI version conflict issue

--- portndock/__init__.py
+++ portndock/__init__.py
@@ -5,7 +5,7 @@ A cross-platform terminal tool for developers to easily find and kill processes
 using specific ports, with special support for Docker containers.
 """
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 __author__ = "Marc Carlo Dy"
 __email__ = "dymarccarlo@yahoo.com"
 
